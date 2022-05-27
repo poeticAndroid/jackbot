ffadb9ee59a83b37c4da740076321be626c88436
poeticAndroid <i@poeticAndroid.online>
2022-05-27 11:57:41 +0200

    phrasing

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function voteGame(channel, tags, message, self) {
     state.gameVotes[state.gameVoters[tags.username]]++
     if (state.state !== "voting") {
       client.say(channel, `@${tags.username} wants to play ${game.title} after this game! SeemsGood`)
-      client.say(channel, `@${tags.username} type '!exit' if you want to play it now.`)
+      client.say(channel, `@${tags.username} type '!exit' if you want to play it right away.`)
     } else {
       client.say(channel, `@${tags.username} wants to play ${game.title}! SeemsGood`)
     }
