bd5818bb252bdd6371a5260029272011b2f82c88
poeticAndroid <i@poeticAndroid.online>
2022-06-20 13:40:29 +0200

    voting stats on screen! pt1

--- web/script.js
+++ web/script.js
@@ -20,9 +20,23 @@ setInterval(() => {
 }, 1024)
 
 function tick(_state) {
+  document.querySelector("#debug").textContent = JSON.stringify(_state, null, 2)
   if (state && (_state.state !== state.state)) {
     document.querySelector("html").classList.remove(state.state)
   }
   state = _state
   document.querySelector("html").classList.add(state.state)
+
+  updateQuitVotes(state.quitVotes, state.state === "quitting")
+}
+
+function updateQuitVotes(votes, count) {
+  document.querySelector("#quit-votes .count").textContent = votes.quit || 0
+  document.querySelector("#restart-votes .count").textContent = votes.restart || 0
+  document.querySelector("#continue-votes .count").textContent = votes.continue || 0
+  if (count) {
+    document.querySelector("#quit-voting meter").value++
+  } else {
+    document.querySelector("#quit-voting meter").value = 0
+  }
 }
