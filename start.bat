069188441d4d54471db2e318518c1f6a48906405
poeticAndroid <i@poeticAndroid.online>
2022-09-26 14:41:18 +0200

    blank lines

--- start.bat
+++ start.bat
@@ -1,9 +1,13 @@
 @echo off
+echo:
 echo update bot ...
 call git pull
+echo:
 echo installing modules ...
 call npm install
+echo:
 echo starting Twitch bot ...
 call node index.js
+echo:
 echo Restarting
 shutdown /r
